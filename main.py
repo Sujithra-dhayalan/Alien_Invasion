a63a3feee06ab396223c86e1563cb960dbaa26d0
Sujithra-dhayalan <sujithradhayalan@gmail.com>
2024-01-19 10:42:25 +0530

    success

---
+++ main.py
@@ -0,0 +1,24 @@
+#import libraries & modules
+import sys
+import pygame
+ 
+def run_game():
+    #initializing the pygame
+    pygame.init()
+
+    #setting screen size, icons, titles of the game
+    screen=pygame.display.set_mode((1200,800))
+    icon=pygame.image.load('ufo.png')
+    pygame.display.set_caption("Alien Invasion")
+    pygame.display.set_icon(icon)
+
+    #creating a loop for the game.
+    run=True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run=False
+        pygame.display.flip()
+        screen.fill((0,0,0))
+run_game()
